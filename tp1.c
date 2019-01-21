2d9c305e427c6a1196a9fff8e0919052d8da4e6d
justinchenmtl <justinchenmtl@gmail.com>
2019-01-21 09:31:31 -0500

    Modification de tp1.c et ajoutez data.txt

--- tp1.c
+++ tp1.c
@@ -1,6 +1,21 @@
 #include <stdio.h> 
  
-int main(int argc, char *argv[]) { 
-    printf("Hello, world!\n"); 
-    return 0; 
+int main() {
+    FILE *fp = NULL;
+    fp = fopen("data.txt", "r"); 
+    int i=0, j=0, min, sum, max;
+    
+    fscanf(fp, "%d", &min);
+    fscanf(fp, "%d", &max);
+  
+    for(i=min; i<=max; i++){
+        sum=0;
+        for(j=1; j<i; j++){
+            if(i%j==0)
+                sum += j;
+        }
+        if(sum == i)
+            printf("Les nombres parfaits sont:  %d\n", i);
+    }
+    return 0;
 } 
