f2463b59df129419aecafd80944c198cbbc8e255
justinchenmtl <justinchenmtl@gmail.com>
2019-01-30 09:37:41 -0500

    Modification de Makefile et tp1.c

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 .PHONY: clean
 
+TARGET = tp1
 OBJECTS = tp1.o
 CC = gcc
 CFLAGS = -Wall -pedantic -std=c99
-tp1: $(OBJECTS)
-	$(CC) $(OBJECTS) o tp1
+SOURCE = https://www.github.com/guyfrancoeur/INF3135_H2019/raw/master/tp1/data.zip
+$(TARGET): $(OBJECTS)
+	$(CC) $(OBJECTS) -o $(TARGET)
 tp1.o: tp1.c
 	$(CC) $(CFLAGS) -c tp1.c -o tp1.o 
 clean:
-	rm -rf $(OBJECTS) tp1
+	rm -rf $(OBJECTS) $(TARGET)
+data:
+	mkdir data
+	wget $(SOURCE)
+	unzip data.zip  -d ./data 
--- tp1.c
+++ tp1.c
@@ -1,12 +1,38 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <string.h> 
  
-int main() {
+int main(int argc, char *argv[]) {
+
     FILE *fp = NULL;
-    fp = fopen("data.txt", "r"); 
+    fp = fopen("./data/data.txt", "r"); 
     int i=0, j=0, min, sum, max;
+
+    for(int i = 0; i < argc; i++){
+        if(argc <= 1 || strcmp(argv[1], "-c") != 0){
+            fprintf(stderr, "Usage: %s <-c CODEpermanent> [-i fichier.in] [-o fichier.out] \n", argv[0]);
+            return 1;
+        }
+        if(strcmp(argv[1], "-c") == 0 && strlen(argv[2]) != 12){
+            return 2;
+        }
+        if(strcmp(argv[1], "-c") != 0 && strlen(argv[2]) != 12){
+            return 3;
+        }
+
+
+
+    return 0;
+
+    }
+    
+    if(fp == NULL){
+        printf("errors!");
+        return -1;
+    }
     
     fscanf(fp, "%d", &min);
     fscanf(fp, "%d", &max);
+    fclose(fp);
   
     for(i=min; i<=max; i++){
         sum=0;
@@ -17,5 +43,6 @@ int main() {
         if(sum == i)
             printf("Les nombres parfaits sont:  %d\n", i);
     }
-    return 0;
+
+    
 } 
