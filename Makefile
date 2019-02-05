ca3741ce9006eade150e0ce44f491910d55ec506
justinchenmtl <justinchenmtl@gmail.com>
2019-02-04 23:19:43 -0500

    Modification des fichiers tp1.c et Makefile

--- Makefile
+++ Makefile
@@ -5,6 +5,8 @@ OBJECTS = tp1.o
 CC = gcc
 CFLAGS = -Wall -pedantic -std=c99
 SOURCE = https://www.github.com/guyfrancoeur/INF3135_H2019/raw/master/tp1/data.zip
+FILE := cp.txt
+CODE := $(shell cat ${FILE})
 $(TARGET): $(OBJECTS)
 	$(CC) $(OBJECTS) -o $(TARGET)
 tp1.o: tp1.c
@@ -14,4 +16,10 @@ clean:
 data:
 	mkdir data
 	wget $(SOURCE)
-	unzip data.zip  -d ./data 
+	unzip data.zip  -d ./data
+test:
+	./tp1 -c $(CODE) -i ./data/data.txt -o resultat.txt
+resultat: 
+	git add resultat.txt
+	git commit -m "Poussez le resultat dans le projet"
+	git push origin master 
--- tp1.c
+++ tp1.c
@@ -1,48 +1,104 @@
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
+#include <stdlib.h> 
  
-int main(int argc, char *argv[]) {
+int main(int argc, char** argv) {
 
-    FILE *fp = NULL;
-    fp = fopen("./data/data.txt", "r"); 
-    int i=0, j=0, min, sum, max;
+    FILE *entree = NULL;
+    FILE *sortie = NULL;
+    FILE *code = NULL; 
+    int i=0, j=0, min, sum, max, count, tete=0, tail=0;
+    count = argc;
 
-    for(int i = 0; i < argc; i++){
-        if(argc <= 1 || strcmp(argv[1], "-c") != 0){
-            fprintf(stderr, "Usage: %s <-c CODEpermanent> [-i fichier.in] [-o fichier.out] \n", argv[0]);
+    for(int i = 0; i < count; i++){
+        if(count <= 2 || count == 4 || count == 6 || strcmp(argv[1], "-c") != 0){
+            fprintf(stderr, "La ligne de commande doit être sous la forme suivante: %s <-c CODEpermanent> [-i fichier.in] [-o fichier.out] \n", argv[0]);
             return 1;
         }
-        if(strcmp(argv[1], "-c") == 0 && strlen(argv[2]) != 12){
-            return 2;
+
+        if(count == 3){
+            entree = stdin;
+            sortie = stdout;
+            if(strcmp(argv[1], "-c") == 0 && strlen(argv[2]) != 12){
+                return 2;
+            }else if(entree == NULL){
+                return 5;
+            }else if(sortie == NULL){
+                return 6;
+            }
         }
-        if(strcmp(argv[1], "-c") != 0 && strlen(argv[2]) != 12){
-            return 3;
-        }
-
-
-
-    return 0;
-
-    }
-    
-    if(fp == NULL){
-        printf("errors!");
-        return -1;
-    }
-    
-    fscanf(fp, "%d", &min);
-    fscanf(fp, "%d", &max);
-    fclose(fp);
   
+        if(count == 5){ 
+            if(strcmp(argv[3], "-i") != 0 && strcmp(argv[3], "-o") != 0){
+                return 3;
+            }else if(strcmp(argv[3], "-i") == 0 ){
+                entree = fopen(argv[4], "r");
+                if(entree == NULL)
+                    return  5;
+            }else if(strcmp(argv[3], "-o") == 0){
+                sortie = fopen(argv[4], "w");
+                if(sortie == NULL)
+                    return 6;
+            }
+            return 0;
+        }
+
+	if(count == 7){
+            if((strcmp(argv[3], "-i") != 0 && strcmp(argv[3], "-o") != 0) || (strcmp(argv[5], "-i") != 0 && strcmp(argv[5], "-o") != 0)){
+	        return 3;
+	    }else if(strcmp(argv[3], "-i") == 0 && strcmp(argv[5], "-o") == 0){
+                entree = freopen(argv[4], "r", stdin);
+                sortie = freopen(argv[6], "w", stdout);
+                if(entree == NULL){
+                    return 5;
+                }else if(sortie == NULL){
+                    return 6;
+                }
+            }else if(strcmp(argv[3], "-o") == 0 && strcmp(argv[5], "-i") == 0){
+                sortie = freopen(argv[4], "w", stdout);
+                entree = freopen(argv[6], "r", stdin);
+                if(entree == NULL){
+                    return 5;
+                }else if(sortie == NULL){
+                    return 6;
+                }
+            }
+	}
+
+        code = fopen("code.txt", "w");
+        fprintf(code, "%s\n", argv[2]);
+        fclose(code);
+
+    }
+
+    scanf("%d", &tete);
+    scanf("%d", &tail);
+
+    if(tete <= 0 || tail <= 0){
+       return 4;
+    }
+
+    if(tete > tail){
+        max = tete;
+        min = tail;
+    }else{
+        max = tail;
+        min = tete;
+    }
+    fclose(entree);
+
     for(i=min; i<=max; i++){
         sum=0;
         for(j=1; j<i; j++){
             if(i%j==0)
                 sum += j;
         }
-        if(sum == i)
-            printf("Les nombres parfaits sont:  %d\n", i);
+        if(sum == i && i != 0){
+
+            printf("%d\n", i);
+        }
     }
 
-    
+
+    return 0;
 } 
